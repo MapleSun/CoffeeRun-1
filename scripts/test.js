88422d432f51407f6143d25533a5c56127088cb2
SYJ <maplesyj@gmail.com>
2017-03-06 21:58:55 -0800

    change the test way to comparing the size of object.

--- scripts/test.js
+++ scripts/test.js
@@ -10,7 +10,8 @@ QUnit.test('8.10 DataStore ', function (assert){
     assert.deepEqual({'m@bond.com':'tea', 'james@bond.com': 'eshpressho'}, ds.getAll(),'getAll function returns an object, passed');
     assert.equal('tea', ds.get('m@bond.com'),'get function passed');
     ds.remove('james@bond.com');
-    assert.ok(undefined === ds.get('james@bond.com'),'remove function passed');
+    var size = Object.keys(ds.getAll()).length;
+    assert.equal(1,size,'remove function passed');
 });
 
 QUnit.test('8.32 Truck', function (assert){
@@ -18,11 +19,14 @@ QUnit.test('8.32 Truck', function (assert){
     myTruck.createOrder({ emailAddress: 'me@goldfinger.com', coffee: 'double mocha'});
     myTruck.createOrder({ emailAddress: 'dr@no.com', coffee: 'decaf'});
     myTruck.createOrder({ emailAddress: 'm@bond.com', coffee: 'earl grey'});
-    myTruck.printOrders();
+    var db_size = Object.keys(myTruck.db.getAll()).length;
+    assert.equal(db_size, 3,'All orders have been added');
     // I change the deliverOrder function to return the output.
     // This make it easy to test the result
-    assert.equal('Delivering order for dr@no.com', myTruck.deliverOrder('dr@no.com'),'Deliver Passed');
-    assert.ok(undefined === myTruck.db.get('dr@no.com'),'dr\'s Order removed');
-    assert.equal('Delivering order for m@bond.com', myTruck.deliverOrder('m@bond.com'), 'Deliver Passed');
-    assert.ok(undefined === myTruck.db.get('m@no.com'),'m\'s Order removed');
+    assert.equal('Delivering order for dr@no.com', myTruck.deliverOrder('dr@no.com'),'dr\'s Deliver Passed');
+    db_size = Object.keys(myTruck.db.getAll()).length;
+    assert.equal(db_size, 2,'dr\'s order removed');
+    assert.equal('Delivering order for m@bond.com', myTruck.deliverOrder('m@bond.com'), 'm\'s Deliver Passed');
+    db_size = Object.keys(myTruck.db.getAll()).length;
+    assert.equal(db_size, 1,'m\'s order removed');
 });
