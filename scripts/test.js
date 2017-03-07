992f7e0e1f891b322d16e70a688d34bd19dd9f1c
SYJ <maplesyj@gmail.com>
2017-03-06 21:35:25 -0800

    improve the documentation of the QUnit tests.

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,4 @@
+
 QUnit.test( 'hello test', function( assert ) {
     assert.ok( 1 == '1', 'Passed!' );
 });
@@ -6,10 +7,10 @@ QUnit.test('8.10 DataStore ', function (assert){
     var ds = window.myTruck.db;
     ds.add('m@bond.com', 'tea');
     ds.add('james@bond.com', 'eshpressho');
-    assert.deepEqual({'m@bond.com':'tea', 'james@bond.com': 'eshpressho'}, ds.getAll());
-    assert.equal('tea', ds.get('m@bond.com'));
+    assert.deepEqual({'m@bond.com':'tea', 'james@bond.com': 'eshpressho'}, ds.getAll(),'getAll function returns an object, passed');
+    assert.equal('tea', ds.get('m@bond.com'),'get function passed');
     ds.remove('james@bond.com');
-    assert.ok(undefined === ds.get('james@bond.com'));
+    assert.ok(undefined === ds.get('james@bond.com'),'remove function passed');
 });
 
 QUnit.test('8.32 Truck', function (assert){
@@ -20,6 +21,8 @@ QUnit.test('8.32 Truck', function (assert){
     myTruck.printOrders();
     // I change the deliverOrder function to return the output.
     // This make it easy to test the result
-    assert.equal('Delivering order for dr@no.com', myTruck.deliverOrder('dr@no.com'));
-    assert.equal('Delivering order for m@bond.com', myTruck.deliverOrder('m@bond.com'));
+    assert.equal('Delivering order for dr@no.com', myTruck.deliverOrder('dr@no.com'),'Deliver Passed');
+    assert.ok(undefined === myTruck.db.get('dr@no.com'),'dr\'s Order removed');
+    assert.equal('Delivering order for m@bond.com', myTruck.deliverOrder('m@bond.com'), 'Deliver Passed');
+    assert.ok(undefined === myTruck.db.get('m@no.com'),'m\'s Order removed');
 });
